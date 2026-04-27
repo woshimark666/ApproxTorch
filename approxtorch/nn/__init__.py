1478096505aab0c5e6d3933b2bfe1c465afbca28
mark <woshimark666@fuji.waseda.jp>
2026-04-27 15:13:26 +0900

    add new layers

--- approxtorch/nn/__init__.py
+++ approxtorch/nn/__init__.py
@@ -3,3 +3,5 @@ from .Conv2d_uint8 import Conv2d_uint8
 from .Conv2d_int8 import Conv2d_int8
 from .FakeQuant import FakeQuantize_int8
 from .Conv2d_gradual_int8 import Conv2d_gradual_int8
+
+from .Conv2dBN import Conv2d_BN_int8
